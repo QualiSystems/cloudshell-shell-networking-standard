afcb975d0ca50e224110f1da4cd96fc26cf1af04
alexquali <alexey.b@quali.com>
2019-06-20 16:40:36 +0300

    Initial

---
+++ cloudshell/shell/standards/networking/autoload_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import cloudshell.shell.standards.attribute_names as attribute_names
+
+from cloudshell.shell.standards.autoload_generic_models import GenericResourceModel, GenericChassis, GenericModule, \
+    GenericSubModule, GenericPort, GenericPowerPort, GenericPortChannel
+from cloudshell.shell.standards.core.autoload.resource_model import ResourceAttribute
+
+__all__ = ['NetworkingResourceModel', 'GenericResourceModel', 'GenericChassis', 'GenericModule', 'GenericSubModule',
+           'GenericPortChannel', 'GenericPowerPort', 'GenericPort']
+
+
+class NetworkingResourceModel(GenericResourceModel):
+    SUPPORTED_FAMILY_NAMES = ['CS_Switch', 'CS_Router', 'CS_WirelessController']
+
+    model_name = ResourceAttribute(attribute_names.MODEL_NAME, ResourceAttribute.NAMESPACE.FAMILY_NAME)
+
+    @property
+    def entities(self):
+        class _NetworkingEntities:
+            Chassis = GenericChassis
+            Module = GenericModule
+            SubModule = GenericSubModule
+            Port = GenericPort
+            PortChannel = GenericPortChannel
+            PowerPort = GenericPowerPort
+
+        return _NetworkingEntities
