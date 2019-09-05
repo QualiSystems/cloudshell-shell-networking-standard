970989442c1f88968885fdd94fd70025b1dd9dc1
Kyrylo Maksymenko <saklar13@gmail.com>
2019-09-05 15:57:42 +0300

    isort

--- cloudshell/shell/standards/networking/resource_config.py
+++ cloudshell/shell/standards/networking/resource_config.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import cloudshell.shell.standards.attribute_names as attribute_names
-
 from cloudshell.shell.standards.core.resource_config_entities import ResourceAttrRO
-from cloudshell.shell.standards.resource_config_generic_models import GenericSnmpConfig, GenericCLIConfig, \
-    GenericConsoleServerConfig, GenericBackupConfig
+from cloudshell.shell.standards.resource_config_generic_models import (
+    GenericBackupConfig,
+    GenericCLIConfig,
+    GenericConsoleServerConfig,
+    GenericSnmpConfig,
+)
 
 
 class NetworkingResourceConfig(GenericSnmpConfig, GenericCLIConfig,
