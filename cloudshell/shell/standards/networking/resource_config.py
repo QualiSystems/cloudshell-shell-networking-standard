36dfc44ce195d309bdffada54930074795429c03
Kyrylo Maksymenko <saklar13@gmail.com>
2023-03-21 10:04:48 +0100

    update pre-commit

--- cloudshell/shell/standards/networking/resource_config.py
+++ cloudshell/shell/standards/networking/resource_config.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 from attrs import define
 
-import cloudshell.shell.standards.attribute_names as attr_name
-from cloudshell.shell.standards.core.resource_conf import attr
 from cloudshell.shell.standards.resource_config_generic_models import (
     GenericBackupConfig,
     GenericCLIConfig,
